075cb56eb039c08cd49c524fc9eab37e8f8d8bc8
Kevin Stradtman <stradtkt22@gmail.com>
2020-06-23 10:43:06 -0400

    Added some components and some functionality

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.modal';
 
 @Component({
@@ -7,12 +7,16 @@ import {Recipe} from '../recipe.modal';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test recipe', 'https://www.justataste.com/wp-content/uploads/2018/01/easy-beef-and-broccoli-recipe-580x875.jpg')
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://www.justataste.com/wp-content/uploads/2018/01/easy-beef-and-broccoli-recipe-580x875.jpg'),
+    new Recipe('Another Test Recipe', 'This is a test recipe', 'https://www.justataste.com/wp-content/uploads/2018/01/easy-beef-and-broccoli-recipe-580x875.jpg')
   ];
   constructor() { }
-
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.modal';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Onion', 5),
+    new Ingredient('Tomato', 3),
+    new Ingredient('Beef', 1)
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
 }
